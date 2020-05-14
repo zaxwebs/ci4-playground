d6e009d18d231bee3bf581c80eabb7200b4b5d70
zaxwebs <zifidigital@gmail.com>
2020-05-14 17:33:06 +0530

    Added quick links

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,5 +1,31 @@
 <div class="container mt-2">
 	<h1>Welcome to CI4 Playground</h1>
 	<p>A space for experimenting the various features of CodeIgniter 4.</p>
-	<a href="/test/test" class="btn btn-primary">Quick Access Test Method</a>
+	<div class="row">
+	<div class="col-md-8 mb-2">
+		<h3>Quick Links</h3>
+		<div class="list-group">
+		<?php
+
+			$links = [
+				'/test/loadHelper' => 'Loading a helper',
+				'/test/formHelper' => 'Form helper demo',
+				'/test/urlHelper' => 'URL helper demo',
+				'/test/escDemo' => 'esc() demo',
+				'/test/loggerDemo' => 'logging demo',
+				'/test/null' => 'null()',
+				'/test/exceptionDemo' => 'exception demo',
+			];
+
+			foreach($links as $link => $title) {
+				echo anchor($link, $title, 'class="list-group-item list-group-item-action"');
+			}
+
+		?>
+		</div>
+	</div>
+	<div class="col-md-4 mb-2">
+		<?= anchor('/test/test', 'Quick Access Test Method', 'class="btn btn-primary"') ?>
+	</div>
+	</div>
 </div>

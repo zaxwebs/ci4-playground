a294260d4402328769fe17097c40f831e9a254f6
zaxwebs <zifidigital@gmail.com>
2020-05-14 12:08:11 +0530

    Added button for Test::test()

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,4 +1,5 @@
 <div class="container mt-2">
 	<h1>Welcome to CI4 Playground</h1>
 	<p>A space for experimenting the various features of CodeIgniter 4.</p>
+	<a href="/test/test" class="btn btn-primary">Quick Access Test Method</a>
 </div>

3de7b9b24f97bd916fd25c1c0caeeed3bd0074a8
zaxwebs <zifidigital@gmail.com>
2020-05-14 12:55:03 +0530

    Added documentation for formHelper()

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -11,9 +11,11 @@ class Test extends BaseController
 	5. Routing updated to Test::index
 	6. Test::index utilizes the newly created view files.
 	7. Added loadHelper()
+	8. Added formHelper()
 	*/
 
-	public function index() {
+	public function index()
+	{
 		echo view('partials/header');
     echo view('index');
     echo view('partials/footer');
@@ -21,12 +23,26 @@ class Test extends BaseController
 
 	public function test()
 	{
+
 	}
 
 	public function loadHelper()
 	{
 		helper('number'); // loads the 'number' helper
 		echo number_to_currency(1234.56, 'USD');
+	}
+
+	public function formHelper()
+	{
+		/*
+		CSRF field can be enabled via before filter in Config/Filters
+		https://codeigniter4.github.io/userguide/libraries/security.html
+		*/
+		helper('form');
+		$attributes = ['class' => 'form', 'id' => 'form'];
+		$hidden = ['origin' => 'home'];
+		$form = form_open('/test', $attributes, $hidden); // saving to variable for dd-ing
+		dd($form); // dump & die
 	}
 
 }
